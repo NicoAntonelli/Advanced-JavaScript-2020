55e5787409b9a3274b4ed384ca4b6840c91dfbfe
Nicolás Antonelli <niconelli2@gmail.com>
2020-05-11 04:09:19 -0300

    Added To-Do List Component

--- ToDo-List-with-Angular/src/app/app.component.ts
+++ ToDo-List-with-Angular/src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ToDo-List-with-Angular';
+  // Few Examples already on the list
+  list = [
+    {description: "Go to the Grocery Store", completed: true},
+    {description: "Study for the Control Theory exam", completed: false},
+    {description: "Buy batteries", completed: true},
+    {description: "Update Python libraries", completed: false},
+    {description: "Add (lots of) CSS to this 'UI'", completed: false}
+  ];
+  // currentTask
+  addTask(inputControl) {
+    if (inputControl.value === "") {
+      return;
+    }
+    this.list.push({
+      description: inputControl.value,
+      completed: false
+    });
+    inputControl.value = "";
+  }
+  remove(index) {
+    // const index = this.list.findIndex(each.description => each.description === item);
+    this.list.splice(index, 1);
+  }
+  toggle(item) {
+    item.completed = !item.completed;
+  }
 }
